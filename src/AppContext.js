b1fddafe0fe9eab6d6c4feb27a4a6cf7a1b00dd1
Nick Balnaves <nickbalnaves@outlook.com>
2018-09-11 20:24:04 +1000

    a

---
+++ src/AppContext.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import firebase from 'firebase/app';
+import { AsyncStorage } from 'react-native'
+
+const AppContext = React.createContext();
+export class AppProvider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    }
+  }
+
+  render() {
+    return <AppContext.Provider value={this.state}>
+      {this.props.children}
+    </AppContext.Provider>
+  }
+}
+export const AppConsumer = AppContext.Consumer;
