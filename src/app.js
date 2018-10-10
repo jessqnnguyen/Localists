1b21f475981f0a1f5813c0591fdba2d99adba284
Jess <jessqnnguyen@gmail.com>
2018-10-11 10:11:49 +1100

    rename App.js to app.js to keep with lowercase file name convention and remove index.css, not using any of the styles from it

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './app.css';
 import NavBar from './components/nav_bar';
 import LoginForm from './components/login_form';
 import Dashboard from './components/dashboard';
