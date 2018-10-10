3c66b02bc0b044b345e1a419200856d0f8a5f72f
robertma1995 <r.ma@student.unsw.edu.au>
2018-10-10 22:40:43 +1100

    moved code into App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
-import { AppProvider } from './AppContext';
-// route-related
-import {Route} from 'react-router-dom';
+import NavBar from './components/nav_bar';
+import LoginForm from './components/login_form';
+import Dashboard from './components/dashboard';
+import RegisterForm from './components/register_form';
+import DiscoverPage from './components/discover_page';
+import CreateListForm from './components/create_list_form';
+import ListPage from './components/list_page';
+import ProfilePage from './components/profile_page';
+// Route related
+import {Switch, Route} from 'react-router-dom';
 import * as routes from './constants/routes';
-// our components
-import Home from './components/home';
-import Discover from './components/discover';
-import NavBar from './components/navbar';
-import LoginForm from './components/loginForm';
-import Profile from './components/profile';
-import ManageLists from './components/manageLists';
+import firebase from 'firebase/app';
+import {
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Button
+} from 'reactstrap';
+require('firebase/auth')
 
+export default class App extends Component {
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar/>
-        <div className="page">
-          <Route exact path={routes.HOME} component={Home}></Route>
-          <Route exact path={routes.DISCOVER} component={Discover}></Route>
-          <Route exact path={routes.LOGIN} component={LoginForm}></Route>
-          <Route exact path={routes.MANAGELISTS} component={ManageLists}></Route>
-          <Route exact path={routes.PROFILE + "/:userId"} component={props => <Profile {...props} />}></Route>
+    render() {
+      return (
+        <div>
+            <NavBar/>
+            <div class="content">
+                <Switch>
+                    <Route exact  path={routes.HOME} component={Dashboard}></Route>
+                    <Route exact path={routes.LOGIN} component={LoginForm}></Route>
+                    <Route exact path={routes.REGISTER} component={RegisterForm}></Route>
+                    <Route exact path={routes.DISCOVER} component={DiscoverPage}></Route>
+                    <Route exact path={routes.CREATELIST} component={CreateListForm}></Route>
+                    <Route exact path={routes.LISTPAGE} component={ListPage}></Route>
+                    <Route exact path={routes.PROFILE} component={ProfilePage}></Route>
+                </Switch>
+            </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
-
-export default App;
