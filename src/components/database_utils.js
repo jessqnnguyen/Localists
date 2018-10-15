b58518d40d6c63964a2c2406a12e9fe85fa6db99
Jess <jessqnnguyen@gmail.com>
2018-10-15 22:35:04 +1100

    fix typo

--- src/components/database_utils.js
+++ src/components/database_utils.js
@@ -12,7 +12,7 @@ export function getUserLists() {
     if (user) {
         return firebase.database().ref("lists/" + user.uid);
     } else {
-        // This function shougld only be called if the user is logged in,
+        // This function should only be called if the user is logged in,
         // throw error if it is called without this condition.
         throw Error("getUserDatabase() failed - user not logged in");
     }
