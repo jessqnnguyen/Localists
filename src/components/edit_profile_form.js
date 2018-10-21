a1475d4533275897bdff0203307573f1e8db2ef5
Jess <jessqnnguyen@gmail.com>
2018-10-21 16:45:33 +1100

    add connections to firebase db

--- src/components/edit_profile_form.js
+++ src/components/edit_profile_form.js
@@ -9,41 +9,87 @@ import GooglePlacesSuggest from "react-google-places-suggest"
 import { AppConsumer } from '../AppContext';
 import LoginForm from './login_form';
 import firebase from 'firebase/app';
+import "firebase/storage";
+import FileUploader from "react-firebase-file-uploader";
 
 class EditProfileForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      uid: '',
       name: '',
       email: '',
-      profileIcon: '',
+      avatarURL: '',
+      password: '',
     };
 
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         const database = firebase.database();
         console.log(user.uid)
+        this.setState({ uid: user.uid });
         const ref = database.ref("users/" + user.uid);
         ref.once("value", (snapshot) => {
           if (snapshot.exists()) {
             const user = snapshot.val();
-            this.setState({ name: user.name, email: user.email });
+            this.setState({ name: user.name, email: user.email, avatarURL: user.avatar });
           }
-          // var l = [];
-          // if (snapshot.exists()) {
-          //   snapshot.forEach((listSnapshot) => {
-          //     l.push({id:listSnapshot.key, title:listSnapshot.val().title, places:listSnapshot.val().places});
-          //   });
-          //   this.setState({ lists: l, loading: false});
-          //   // console.log(this.state.lists, l);
-          // } else {
-          //   // No lists found, loading done, set loading to false
-          //   this.setState({ loading: false });
-          // }
         });
       }
     });
   }
+
+  updatePassword(newPassword) {
+    this.setState({ password: newPassword});
+    
+  }
+
+  saveChanges() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // Update user's name and email and avatar
+        var db = firebase.database();
+        db.ref('users/' + user.uid).set({
+          name: this.state.name,
+          email: this.state.email,
+          avatar: this.state.avatarURL,
+        });
+
+        // If the user has updated their password, the state should store the new password
+        // Else if it is empty the user hasn't updated their password
+        // if (this.state.password !== '') {
+        //   console.log("the state's password is " + this.state.password);
+        //   console.log("this ran but it shouldn't have");
+          
+        //   user.updatePassword(this.state.password).then(function() {
+        //     // Update successful
+        //     this.props.history.push(routes.HOME);
+        //   }).catch(function(error) {
+        //     throw new Error("Error: Password could not be updated");
+        //   });
+        // }
+      }
+      this.props.history.push(routes.HOME);
+    });
+  }
+
+  handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
+  handleProgress = progress => this.setState({ progress });
+  
+  handleUploadError = error => {
+    this.setState({ isUploading: false });
+    console.error(error);
+  };
+ 
+  handleUploadSuccess = filename => {
+    this.setState({ avatar: filename, progress: 100, isUploading: false });
+    firebase
+      .storage()
+      .ref("images")
+      .child(filename)
+      .getDownloadURL()
+      .then(url => this.setState({ avatarURL: url }));
+  };
 
   render() {
     return (
@@ -65,17 +111,31 @@ class EditProfileForm extends Component {
                 </FormGroup>
                 <FormGroup>
                   <Label for="examplePassword">Change password</Label>
-                  <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                  <Input type="password" name="password" id="examplePassword" value={this.state.password} onChange={event => this.setState({ password: event.target.value })}/>
                 </FormGroup>
                 <FormGroup>
                   <Label for="exampleFile">Upload a profile picture</Label>
-                  <Input type="file" name="file" id="exampleFile" />
+                  {this.state.isUploading && <p>Progress: {this.state.progress}</p>}
+                  {this.state.avatarURL && <img src={this.state.avatarURL} />}
+                  <div class="editProfileUploadImageButton">
+                    <FileUploader
+                      accept="image/*"
+                      name="avatar"
+                      filename={file => this.state.uid }
+                      storageRef={firebase.storage().ref('images')}
+                      onUploadStart={this.handleUploadStart}
+                      onUploadError={this.handleUploadError}
+                      onUploadSuccess={this.handleUploadSuccess}
+                      onProgress={this.handleProgress}
+                    />
+                  </div>
+                  {/* <Input type="file" name="file" id="exampleFile" /> */}
                   <FormText color="muted">
                     Please upload a square image.
                   </FormText>
                 </FormGroup>
                 <div class="editProfileSaveChangesButton">
-                  <Button color="primary" size="lg" onClick={() => console.log('blah')}>Save changes</Button>
+                  <Button color="primary" size="lg" onClick={() => this.saveChanges()}>Save changes</Button>
                 </div>
               </Form>
             </div>
